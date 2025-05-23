9b5141acbb69b305b41fe75be32ec5eb63e1a373
Lawrence de Varga <lawrencedevarga@gmail.com>
2025-05-23 21:42:29 +1000

    pygame screen

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pygame
+import constants as cn
+
+pygame.init()
+screen = pygame.display.set_mode((cn.SCREEN_WIDTH, cn.SCREEN_HEIGHT))
+
+
 def main():
-    print("Hello from asteroids!")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
