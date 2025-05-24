79e0d4e234c3f7131e0a03971bf4950bb00f761e
Lawrence de Varga <lawdevarga@gmail.com>
2025-05-24 14:49:11 +1000

    Added player to groups.

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ import pygame
 import constants as cn
 from player import Player
 
-pygame.init()
-screen = pygame.display.set_mode((cn.SCREEN_WIDTH, cn.SCREEN_HEIGHT))
-
-# Clock
-clock = pygame.time.Clock()
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((cn.SCREEN_WIDTH, cn.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(cn.SCREEN_WIDTH / 2, cn.SCREEN_HEIGHT / 2)
+
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updateable.update(dt)
+        
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
